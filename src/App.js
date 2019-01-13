63830a6832d77dc9d09983a82044c760037ebe8b
Patrick McMahon <pmcmahon14@gmail.com>
2019-01-12 19:50:14 -0800

    added background image

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+          <img className="background" src={require("./components/Images/background/Jimmie_2048x1309.jpg")}/>
           <Menu />
           {/*<Stats />
           <Gameboard />*/}
