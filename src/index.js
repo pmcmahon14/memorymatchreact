a962dce89bda424c52b68d938bacc0265c2e7a02
Patrick McMahon <pmcmahon14@gmail.com>
2019-01-30 18:59:08 -0800

    added images.js for cards and added styling to Chase for design/test purposes

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Card from './components/Card/Card';
 import * as serviceWorker from './serviceWorker';
+import 'tachyons';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
     <div>
