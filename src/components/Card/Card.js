52a68c421f5fbe5aa752fa9b0190f646a85b026a
Patrick McMahon <pmcmahon14@gmail.com>
2019-01-30 18:01:06 -0800

    cleared out unused files and started card component

---
+++ src/components/Card/Card.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div>
+            <img alt='photo' src='' />
+            <div>
+            <h2>driver</h2>
+            </div>
+        </div>
+    )
+};
+
+export default Card;
