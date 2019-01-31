542c298e1d3b7236945f31fa58863e4d0d994a69
Patrick McMahon <pmcmahon14@gmail.com>
2019-01-30 20:16:31 -0800

    added images.js, includes pix off all drivers

---
+++ src/images.js
@@ -0,0 +1,167 @@
+export const images = [
+    {
+        id: 1,
+        driver: 'aricalmirola.png',
+        car: 10
+    },
+    {
+        id: 2,
+        driver: 'ryanblaney.png',
+        car: 12
+    },
+    {
+        id: 3,
+        driver: 'alexbowman.png',
+        car: 88
+    },
+    {
+        id: 4,
+        driver: 'clintbowyer.png',
+        car: 14
+    },
+    {
+        id: 5,
+        driver: 'chrisbuechser.png',
+        car: 37
+    },
+    {
+        id: 6,
+        driver: 'kylebusch.png',
+        car: 18
+    },
+    {
+        id: 7,
+        driver: 'kurtbusch.png',
+        car: 1
+    },
+    {
+        id: 8,
+        driver: 'williambyron.png',
+        car: 24
+    },
+    {
+        id: 9,
+        driver: 'landoncassill.png',
+        car: 00
+    },
+    {
+        id: 10,
+        driver: 'rosschastain.png',
+        car: 15
+    },
+    {
+        id: 11,
+        driver : 'mattdibenedetto.png',
+        car: 95
+    },
+    {
+        id: 12,
+        driver: 'austindillon.png',
+        car: 3
+    },
+    {
+        id: 13,
+        driver: 'tydillon.png',
+        car: 13
+    },
+    {
+        id: 14,
+        driver: 'chaseelliott.png',
+        car: 9
+    },
+    {
+        id: 15,
+        driver: 'dennyhamlin.png',
+        car: 11
+    },
+    {
+        id: 16,
+        driver: 'kevinharvick.png',
+        car: 4
+    },
+    {
+        id: 17,
+        driver: 'danielhemric.png',
+        car: 8
+    },
+    {
+        id: 18,
+        driver: 'jimmiejohnson.png',
+        car: 48
+    },
+    {
+        id: 19,
+        driver: 'erikjones.png',
+        car: 20
+    },
+    {
+        id: 20,
+        driver: 'bradkeselowski.png',
+        car: 2
+    },
+    {
+        id: 21,
+        driver: 'coreylajoie.png',
+        car: 32
+    },
+    {
+        id: 22,
+        driver: 'kylelarson.png',
+        car: 42
+    },
+    {
+        id: 23,
+        driver: 'joeylogano.png',
+        car: 22
+    },
+    {
+        id: 24,
+        driver: 'michaelmcdowell.png',
+        car: 34
+    },
+    {
+        id: 25,
+        driver: 'paulmenard.png',
+        car:21
+    },
+    {
+        id: 26,
+        driver: 'ryannewman.png',
+        car: 6
+    },
+    {
+        id: 27,
+        driver: 'ryanpreece.png',
+        car: 47
+    },
+    {
+        id: 28,
+        driver: 'davidragan.png',
+        car: 38
+    },
+    {
+        id: 29,
+        driver: 'rickystenhouse.png',
+        car: 17
+    },
+    {
+        id: 30,
+        driver: 'danielsuarez.png',
+        car: 41
+    },
+    {
+        id: 31,
+        driver: 'matttifft.png',
+        car: 36
+    },
+    {
+        id: 32,
+        driver: 'martintruex.png',
+        car: 19
+    },
+    {
+        id: 33,
+        driver: 'darrellwallace.png',
+        car: 43
+    }
+];
