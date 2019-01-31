bd25df3e571ed3a08787f14167bb135cfde8c534
Patrick McMahon <pmcmahon14@gmail.com>
2019-01-30 20:36:06 -0800

    fixed naming conflict with directory and js file 'images' and began destructuring with cards

--- src/index.js
+++ src/index.js
@@ -5,12 +5,13 @@ import Card from './components/Card/Card';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {images} from './images';
 
 ReactDOM.render(
     <div>
-        <Card />
-        <Card />
-        <Card />
+        <Card driver={images[0].driver}/>
+        <Card driver={images[3].driver}/>
+        <Card driver={images[6].driver}/>
     </div>
         , document.getElementById('root'));
 
