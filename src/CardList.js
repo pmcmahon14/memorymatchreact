fae827d916915a3f0452d8a2e7556795b58ef055
Patrick McMahon <pmcmahon14@gmail.com>
2019-01-31 16:07:15 -0800

    added cardComponent to CardList.js, map over each image object to return each driver and car number

--- src/CardList.js
+++ src/CardList.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Card from './Card';
 
 const CardList = ({images}) => {
+    const cardComponent = images.map((user, i) => {
+        return <Card driver={images[i].driver} car={images[i].car}/>
+    });
     return (
         <div>
-            <Card driver={images[0].driver} car={images[0].car}/>
-            <Card driver={images[1].driver} car={images[1].car}/>
-            <Card driver={images[2].driver} car={images[2].car}/>
+            {cardComponent}
         </div>
     )
 };
