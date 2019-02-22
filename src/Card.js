068767f19f2654c20da37abc9b2998f6e646c758
Patrick McMahon <pmcmahon14@gmail.com>
2019-02-22 13:47:30 -0800

    installed reactstrap and set up preliminary playing card in Card.js

--- src/Card.js
+++ src/Card.js
@@ -1,16 +1,26 @@
 import React from 'react';
 //import Container from './Container';
+import {Container, Row, Col} from 'reactstrap';
+import './index.css';
 
 const Card = ({driver, car}) => {
     return (
-        //tachyons here only for development
-        <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
-            <img alt='card' src={require(`./${driver}`)} />
+        <Container fluid>
+            {/*tachyons here only for development*/}
+        <div className='tc dib br3 pa3 ma2 grow bw2 shadow-5'>
+            <Row>
+                <Col xs="2">
             <div>
-                <p>{car}</p>
+            <img alt='card' src={require(`./${driver}`)} />
             </div>
-            <img alt='logo' src={require(`./imagebank/logo.png`)}/>
+            <div>
+                <img className='back' alt='logo' src={require(`./imagebank/logo.png`)}/>
+                {/*<p>{car}</p>*/}
+            </div>
+                </Col>
+            </Row>
         </div>
+        </Container>
     )
 };
 
