918d692a0bd1ce47bab41c63f2d03fbde67c4b7b
Patrick McMahon <pmcmahon14@gmail.com>
2019-04-14 20:32:06 -0700

    changed driver image references to images from car numbers

--- src/images.js
+++ src/images.js
@@ -3,166 +3,166 @@ export const images =
     {
         id: 1,
         driver: 'imagebank/drivers/aricalmirola.png',
-        car: '10'
+        car: 'imagebank/cars/aricalmirola.jpg'
     },
     {
         id: 2,
         driver: 'imagebank/drivers/ryanblaney.png',
-        car: '12'
+        car: 'imagebank/cars/ryanblaney.jpg'
     },
     {
         id: 3,
         driver: 'imagebank/drivers/alexbowman.png',
-        car: '88'
+        car: 'imagebank/cars/alexbowman.jpg'
     },
     {
         id: 4,
         driver: 'imagebank/drivers/clintbowyer.png',
-        car: '14'
+        car: 'imagebank/cars/clintbowyer.jpg'
     },
     {
         id: 5,
         driver: 'imagebank/drivers/chrisbuechser.png',
-        car: '37'
+        car: 'imagebank/cars/chrisbuechser.jpg'
     },
     {
         id: 6,
         driver: 'imagebank/drivers/kylebusch.png',
-        car: '18'
+        car: 'imagebank/cars/kylebusch.jpg'
     },
     {
         id: 7,
         driver: 'imagebank/drivers/kurtbusch.png',
-        car: '1'
+        car: 'imagebank/cars/kurtbusch.jpg'
     },
     {
         id: 8,
         driver: 'imagebank/drivers/williambyron.png',
-        car: '24'
+        car: 'imagebank/cars/williambyron.jpg'
     },
     {
         id: 9,
         driver: 'imagebank/drivers/landoncassill.png',
-        car: '00'
+        car: 'imagebank/cars/landoncassill.jpg'
     },
     {
         id: 10,
         driver: 'imagebank/drivers/rosschastain.png',
-        car: '15'
+        car: 'imagebank/cars/rosschastain.jpg'
     },
     {
         id: 11,
         driver : 'imagebank/drivers/mattdibenedetto.png',
-        car: '95'
+        car: 'imagebank/cars/mattdibenedetto.jpg'
     },
     {
         id: 12,
         driver: 'imagebank/drivers/austindillon.png',
-        car: '3'
+        car: 'imagebank/cars/austindillon.jpg'
     },
     {
         id: 13,
         driver: 'imagebank/drivers/tydillon.png',
-        car: '13'
+        car: 'imagebank/cars/tydillon.jpg'
     },
     {
         id: 14,
         driver: 'imagebank/drivers/chaseelliott.png',
-        car: '9'
+        car: 'imagebank/cars/chaseelliott.jpg'
     },
     {
         id: 15,
         driver: 'imagebank/drivers/dennyhamlin.png',
-        car: '11'
+        car: 'imagebank/cars/dennyhamlin.jpg'
     },
     {
         id: 16,
         driver: 'imagebank/drivers/kevinharvick.png',
-        car: '4'
+        car: 'imagebank/cars/kevinharvick.jpg'
     },
     {
         id: 17,
         driver: 'imagebank/drivers/danielhemric.png',
-        car: '8'
+        car: 'imagebank/cars/danielhemric.jpg'
     },
     {
         id: 18,
         driver: 'imagebank/drivers/jimmiejohnson.png',
-        car: '48'
+        car: 'imagebank/cars/jimmiejohnson.jpg'
     },
     {
         id: 19,
         driver: 'imagebank/drivers/erikjones.png',
-        car: '20'
+        car: 'imagebank/cars/erikjones.jpg'
     },
     {
         id: 20,
         driver: 'imagebank/drivers/bradkeselowski.png',
-        car: '2'
+        car: 'imagebank/cars/bradkeselowski.jpg'
     },
     {
         id: 21,
         driver: 'imagebank/drivers/coreylajoie.png',
-        car: '32'
+        car: 'imagebank/cars/coreylajoie.jpg'
     },
     {
         id: 22,
         driver: 'imagebank/drivers/kylelarson.png',
-        car: '42'
+        car: 'imagebank/cars/kylelarson.jpg'
     },
     {
         id: 23,
         driver: 'imagebank/drivers/joeylogano.png',
-        car: '22'
+        car: 'imagebank/cars/joeylogano.jpg'
     },
     {
         id: 24,
         driver: 'imagebank/drivers/michaelmcdowell.png',
-        car: '34'
+        car: 'imagebank/cars/michaelmcdowell.jpg'
     },
     {
         id: 25,
         driver: 'imagebank/drivers/paulmenard.png',
-        car: '21'
+        car: 'imagebank/cars/paulmenard.jpg'
     },
     {
         id: 26,
         driver: 'imagebank/drivers/ryannewman.png',
-        car: '6'
+        car: 'imagebank/cars/ryannewman.jpg'
     },
     {
         id: 27,
         driver: 'imagebank/drivers/ryanpreece.png',
-        car: '47'
+        car: 'imagebank/cars/ryanpreece.jpg'
     },
     {
         id: 28,
         driver: 'imagebank/drivers/davidragan.png',
-        car: '38'
+        car: 'imagebank/cars/davidragan.jpg'
     },
     {
         id: 29,
         driver: 'imagebank/drivers/rickystenhouse.png',
-        car: '17'
+        car: 'imagebank/cars/rickystenhouse.jpg'
     },
     {
         id: 30,
         driver: 'imagebank/drivers/danielsuarez.png',
-        car: '41'
+        car: 'imagebank/cars/danielsuarez.jpg'
     },
     {
         id: 31,
         driver: 'imagebank/drivers/matttifft.png',
-        car: '36'
+        car: 'imagebank/cars/matttifft.jpg'
     },
     {
         id: 32,
         driver: 'imagebank/drivers/martintruex.png',
-        car: '19'
+        car: 'imagebank/cars/martintruex.jpg'
     },
     {
         id: 33,
         driver: 'imagebank/drivers/bubbawallace.png',
-        car: '43'
+        car: 'imagebank/cars/bubbawallace.jpg'
     }
 ];
