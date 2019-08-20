306ea51f9fa1c07574dca1fd1831a00c18c14aae
Patrick McMahon <pmcmahon14@gmail.com>
2019-08-20 16:40:23 -0700

    installed redux libraries

--- src/components/Gameboard/Gameboard.js
+++ src/components/Gameboard/Gameboard.js
@@ -3,97 +3,20 @@ import {Container, Row, Col} from 'reactstrap';
 import PropTypes from 'prop-types';
 
 import Card from '../Card/Card';
-import classes from './Gameboard.module.css';
+import './Gameboard.module.css';
 
 export default function Gameboard({ cards, flipped, handleClick, disabled, solved }) {
-    {/*return (
+    const width = 2;
+
+    return (
             <div>
                 <Container fluid>
-                    <Row className={classes.rowDaytona}>
-                        <Col className='grow'>
-                            <img className={classes.front} alt='card' src={require('../../imagebank/drivers/alexbowman.png')}/>
-                            <img className='back' alt='cardback' src={require('../../imagebank/logo.png')}/>
-                        </Col>
-                        <Col xs='2' className='grow'>
-                            <Card
-                                className={classes.front}
-                                alt='card'
-                                back={require(`../../imagebank/logo.png`)}
-                                front={require(`../../imagebank/drivers/alexbowman.png`)}
-                                flipped={flipped.includes(1)}
-                                handleClick={() => handleClick(1)}
-                                />
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/alexbowman.png')}/>
-                            <img className={classes.back} alt='cardback' src={require('../../imagebank/logo.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/chaseelliott.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/chaseelliott.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/jimmiejohnson.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/jimmiejohnson.png')}/>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/austindillon.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/austindillon.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/kevinharvick.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/kevinharvick.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/martintruex.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/martintruex.png')}/>
-                        </Col>
-                    </Row>
-                    <Row>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/joeylogano.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/joeylogano.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/tydillon.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/tydillon.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/aricalmirola.png')}/>
-                        </Col>
-                        <Col className='grow'>
-                            <img className='front' alt='card' src={require('../../imagebank/drivers/aricalmirola.png')}/>
-                        </Col>
-                    </Row>
-                </Container>
-            </div>
-        )*/}
-        return (
-            <div>
-                <Container fluid>
-                    <Row className='justify-conteent-around'>
+                    <Row className='justify-content-around rowDaytona'>
                     {cards.map((card) => (
-                        <Col xs='2' className='grow'>
+                        <Col xs={width} className='grow'>
                             <Card
                                 key={card.id}
                                 id={card.id}
-                                className={classes.front}
                                 alt='card'
                                 flipped={flipped.includes(card.id)}
                                 handleClick={handleClick}
